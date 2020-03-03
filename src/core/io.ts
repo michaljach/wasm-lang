7a30d5d15ec5a1e635f8fed764c6a5124e94019a
Michal Jach <michaljach@gmail.com>
2020-03-03 16:47:34 +0000

    docs: ✏️ added descriptions for all commands

--- src/core/io.ts
+++ src/core/io.ts
@@ -7,9 +7,9 @@ export interface Args {
 }
 
 export const { argv }: { argv: Args } = yargs.options({
-  f: { type: 'string', demandOption: true, alias: 'file', nargs: 1 },
-  o: { type: 'string', demandOption: true, alias: 'output', nargs: 1 },
-  s: { type: 'boolean', alias: 'sourceMaps' },
+  f: { type: 'string', demandOption: true, alias: 'file', nargs: 1, description: 'Path to source file to be compiled' },
+  o: { type: 'string', demandOption: true, alias: 'output', nargs: 1, description: 'Path to result file to be saved' },
+  s: { type: 'boolean', alias: 'sourceMaps', description: 'Generate sourceMaps for debugging' },
 });
 
 export const save = (fileName: string, buffer: string | Uint8Array): void => {
